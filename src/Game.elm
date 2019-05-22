fb058b6ad573e773d5ee534d57a05d9a16cb2d57
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2019-05-23 00:07:28 +0200

    Move game logic into Game module

--- src/Game.elm
+++ src/Game.elm
@@ -1,7 +1,18 @@
-module Game exposing (Data)
+module Game exposing (Data, State, strategy)
 
-import Data.Map exposing (Map)
+import Array exposing (Array)
+import Data.Cell as Cell
+import Data.Map as Map exposing (Map)
 import Data.Shared exposing (..)
+import Dict exposing (Dict)
+import Process.Frontier as Frontier
+import Process.Movement as Movement
+import Process.Training as Training
+
+
+type alias State =
+    { minesSpots : Array Pos
+    }
 
 
 type alias Data =
@@ -14,3 +25,81 @@ type alias Data =
     , buildings : List Building
     , units : List Unit
     }
+
+
+strategy : Data -> State -> ( State, String, String )
+strategy data state =
+    let
+        enemyHqPos =
+            case Map.get 0 0 data.map of
+                Just (Cell.Active Me _) ->
+                    { x = 11, y = 11 }
+
+                _ ->
+                    { x = 0, y = 0 }
+
+        ( myUnits, enemyUnits ) =
+            List.partition (\u -> u.owner == Me) data.units
+
+        ( myBuildings, enemyBuildings ) =
+            List.partition (\b -> b.owner == Me) data.buildings
+
+        updatedMapWithUnits =
+            List.foldl Map.updateUnit data.map data.units
+
+        updatedMapWithBuildings =
+            List.foldl Map.updateBuilding updatedMapWithUnits data.buildings
+
+        ( newMap, movements ) =
+            Movement.compute enemyHqPos updatedMapWithBuildings myUnits
+
+        sortedMovements =
+            List.reverse movements
+
+        frontier =
+            Frontier.compute newMap
+
+        frontierPosString =
+            Dict.keys frontier
+                |> List.map (\( x, y ) -> String.fromInt x ++ ", " ++ String.fromInt y)
+                |> String.join "\n"
+
+        training =
+            Training.compute newMap frontier
+
+        sortedTraining =
+            Training.sort enemyHqPos newMap training
+
+        trainingString =
+            List.map (\t -> List.map String.fromInt [ t.level, t.x, t.y ]) training
+                |> List.map (String.join " ")
+                |> String.join "\n"
+
+        trainingComparableString =
+            List.map (Training.comparable enemyHqPos newMap) training
+                |> List.map (\( a, b, c ) -> [ String.fromInt a, String.fromInt b, String.fromInt c ])
+                |> List.map (String.join " ")
+                |> String.join "\n"
+
+        paidTraining =
+            Training.spend data.gold sortedTraining []
+
+        theOrders =
+            String.join ";" <|
+                List.concat
+                    [ List.map Movement.order sortedMovements
+                    , List.map Training.order paidTraining
+                    , [ "WAIT" ]
+                    ]
+
+        log =
+            String.join "\n\n" <|
+                [ "Turn: " ++ String.fromInt data.turn
+                , "Training:\n" ++ trainingString
+                , "TrainingComparable:\n" ++ trainingComparableString
+                , "Frontier:\n" ++ frontierPosString
+
+                -- , "Map:\n" ++ Map.toString newMap
+                ]
+    in
+    ( state, theOrders, log )
--- src/Main.elm
+++ src/Main.elm
@@ -1,18 +1,12 @@
 port module Main exposing (main)
 
 import Array exposing (Array)
-import Data.Cell as Cell exposing (Cell)
-import Data.Map as Map exposing (Map)
-import Data.Shared exposing (..)
 import Decode
-import Dict exposing (Dict)
+import Game
 import Json.Decode as Decode exposing (Decoder, Value)
-import Process.Frontier as Frontier
-import Process.Movement as Movement
-import Process.Training as Training
 
 
-main : Program Value Model Value
+main : Program Value Game.State Value
 main =
     Platform.worker
         { init = init
@@ -40,107 +34,29 @@ port debug : String -> Cmd msg
 the Value contains initial game data.
 Extract with a Json decoder.
 -}
-init : Value -> ( Model, Cmd msg )
+init : Value -> ( Game.State, Cmd msg )
 init data =
     case Decode.decodeValue Decode.initData data of
         Ok minesSpots ->
-            ( Model minesSpots, debug "Init Done!" )
+            ( Game.State minesSpots, debug "Init Done!" )
 
         Err error ->
-            ( Model Array.empty, debug (Decode.errorToString error) )
+            ( Game.State Array.empty, debug (Decode.errorToString error) )
 
 
 {-| Function called during game loop with game data of current turn.
 Extract the data with a Json decoder.
 -}
-update : Value -> Model -> ( Model, Cmd msg )
-update data model =
+update : Value -> Game.State -> ( Game.State, Cmd msg )
+update data state =
     case Decode.decodeValue Decode.gameData data of
         Ok gameData ->
             let
-                enemyHqPos =
-                    case Map.get 0 0 gameData.map of
-                        Just (Cell.Active Me _) ->
-                            { x = 11, y = 11 }
-
-                        _ ->
-                            { x = 0, y = 0 }
-
-                ( myUnits, enemyUnits ) =
-                    List.partition (\u -> u.owner == Me) gameData.units
-
-                ( myBuildings, enemyBuildings ) =
-                    List.partition (\b -> b.owner == Me) gameData.buildings
-
-                updatedMapWithUnits =
-                    List.foldl Map.updateUnit gameData.map gameData.units
-
-                updatedMapWithBuildings =
-                    List.foldl Map.updateBuilding updatedMapWithUnits gameData.buildings
-
-                ( newMap, movements ) =
-                    Movement.compute enemyHqPos updatedMapWithBuildings myUnits
-
-                sortedMovements =
-                    List.reverse movements
-
-                frontier =
-                    Frontier.compute newMap
-
-                frontierPosString =
-                    Dict.keys frontier
-                        |> List.map (\( x, y ) -> String.fromInt x ++ ", " ++ String.fromInt y)
-                        |> String.join "\n"
-
-                training =
-                    Training.compute newMap frontier
-
-                sortedTraining =
-                    Training.sort enemyHqPos newMap training
-
-                trainingString =
-                    List.map (\t -> List.map String.fromInt [ t.level, t.x, t.y ]) training
-                        |> List.map (String.join " ")
-                        |> String.join "\n"
-
-                trainingComparableString =
-                    List.map (Training.comparable enemyHqPos newMap) training
-                        |> List.map (\( a, b, c ) -> [ String.fromInt a, String.fromInt b, String.fromInt c ])
-                        |> List.map (String.join " ")
-                        |> String.join "\n"
-
-                paidTraining =
-                    Training.spend gameData.gold sortedTraining []
-
-                theOrders =
-                    String.join ";" <|
-                        List.concat
-                            [ List.map Movement.order sortedMovements
-                            , List.map Training.order paidTraining
-                            , [ "WAIT" ]
-                            ]
-
-                log =
-                    String.join "\n\n" <|
-                        [ "Turn: " ++ String.fromInt gameData.turn
-                        , "Training:\n" ++ trainingString
-                        , "TrainingComparable:\n" ++ trainingComparableString
-                        , "Frontier:\n" ++ frontierPosString
-
-                        -- , "Map:\n" ++ Map.toString newMap
-                        ]
+                ( newState, theOrders, log ) =
+                    Game.strategy gameData state
             in
-            -- ( model, Cmd.batch [ order theOrders ] )
-            ( model, Cmd.batch [ debug log, order theOrders ] )
+            -- ( state, Cmd.batch [ order theOrders ] )
+            ( newState, Cmd.batch [ debug log, order theOrders ] )
 
         Err error ->
-            ( model, debug (Decode.errorToString error) )
-
-
-
--- Types
-
-
-type alias Model =
-    { minesSpots : Array Pos
-    }
+            ( state, debug (Decode.errorToString error) )
